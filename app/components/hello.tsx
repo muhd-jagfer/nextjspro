26c190143b3404700e58d06a37c004e14c97820e
muhd-jagfer <muhdjagfer@gmail.com>
2026-08-12 22:31:33 +0530

    using client side and server side rendering

---
+++ app/components/hello.tsx
@@ -0,0 +1,9 @@
+'use client';
+const Hello = () => {
+    console.log(`iam a client side component...`)
+    return(
+        <div>Hello</div>
+    )
+};
+
+export default Hello;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import Hello from "./components/hello";
+
 const Home = () => {
+  console.log(`what type of a component am i`)
   return(
+  <main>
     <div className="text-5xl underline bold"> Welcome to Next.js !</div>
+    <Hello />
+  </main>
   )
 };
 export default Home;
